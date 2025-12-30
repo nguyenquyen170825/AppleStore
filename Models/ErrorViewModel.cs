b27cffd020b21d09bb190af1d3344e39baa53e6a
NguyenQuyen <quyenketvl@gmail.com>
2025-12-30 19:50:55 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DUANCUAHANGAPPLE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
