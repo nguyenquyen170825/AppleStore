ee2b565a82a56b46f1f89f55c2783ca88adc0842
NguyenQuyen <quyenketvl@gmail.com>
2026-01-10 19:23:34 +0700

    quyen

--- Controllers/sanphamController.cs
+++ Controllers/sanphamController.cs
@@ -4,12 +4,6 @@ namespace DUANCUAHANGAPPLE.Controllers
 {
     public class SanPhamController : Controller
         {
-            private readonly ApplicationDbContext _context;
-            public SanPhamController(ApplicationDbContext context)
-            {
-                _context = context;
-            }
-
             
         }
 }
